7a54b93edce2848eb91a9dad0fac512185860783
JoeyChiou <joeyc0916@gmail.com>
2015-04-22 01:49:43 +0800

    Project Generation

---
+++ TestFlow.java
@@ -0,0 +1,10 @@
+public class TestFlow{
+	public static void main(String args[]){
+		System.out.println("test");
+		int x = 1;
+		int y = 2;
+		twoADD(x, y);
+		twoMin(x, y);
+		twoMax(x, y);
+	}
+}
